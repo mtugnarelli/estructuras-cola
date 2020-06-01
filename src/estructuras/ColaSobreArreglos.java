31240efdcb49505e8e6832e4d52d9a6ac24a93b4
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-06-01 02:06:20 -0300

    ColaSobreArreglos

---
+++ src/estructuras/ColaSobreArreglos.java
@@ -0,0 +1,31 @@
+package estructuras;
+
+/**
+ * Implementación de Cola (Queue) basada en arreglos.
+ * 
+ */
+public class ColaSobreArreglos<T> implements Cola<T> {
+
+	@Override
+	public boolean estaVacia() {
+
+		return false;
+	}
+
+	@Override
+	public void acolar(T nuevoElemento) {
+		
+	}
+
+	@Override
+	public T desacolar() {
+
+		return null;
+	}
+
+	@Override
+	public T obtenerFrente() {
+
+		return null;
+	}
+}
